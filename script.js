ef65fa724888ffb7f78b963e4e69b9fdb0a6fd35
siddhant shrivastava <siddhant.shrivastava@mountblue.io>
2018-06-30 00:38:07 +0530

    first commit without flip css effect

---
+++ script.js
@@ -0,0 +1,67 @@
+var memory_array = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L'];
+var memory_values = [];
+var memory_tile_ids = [];
+var tiles_flipped = 0;
+Array.prototype.memory_tile_shuffle = function () {
+    var i = this.length,
+        j, temp;
+    while (--i > 0) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+    }
+}
+
+//creating memory board
+function newBoard() {
+    tiles_flipped = 0;
+    var output = '';
+    memory_array.memory_tile_shuffle();
+    for (let i = 0; i < memory_array.length; i++) {
+        output += '<div id="tile_' + i + '" onclick="memoryFlipTile(this,\'' + memory_array[i] + '\')"></div>';
+    }
+    document.getElementById('memory_board').innerHTML = output;
+}
+newBoard();
+
+//function to flip tiles
+function memoryFlipTile(tile, val) {
+    if (tile.innerHTML == "" && memory_values.length < 2) {
+        tile.style.background = '#FFF';
+        tile.innerHTML = val;
+        if (memory_values.length == 0) {
+            memory_values.push(val);
+            memory_tile_ids.push(tile.id);
+        } else if (memory_values.length == 1) {
+            memory_values.push(val);
+            memory_tile_ids.push(tile.id);
+            if (memory_values[0] == memory_values[1]) {
+                tiles_flipped += 2;
+                //clear both aarays
+                memory_values = [];
+                memory_tile_ids = [];
+                //check to see if the whole board is cleared
+                if (tiles_flipped == memory_array.length) {
+                    alert('Board cleared... generating new board');
+                    document.getElementById('memory_board').innerHTML = "";
+                    newBoard();
+                }
+            } else {
+                function flip2Back() {
+                    //Flip the tiles back over
+                    var tile_1 = document.getElementById(memory_tile_ids[0]);
+                    var tile_2 = document.getElementById(memory_tile_ids[1]);
+                    tile_1.style.background = 'URL(mosaic.png)';
+                    tile_1.innerHTML = "";
+                    tile_2.style.background = 'URL(mosaic.png)';
+                    tile_2.innerHTML = "";
+                    //clear both arrays
+                    memory_values = [];
+                    memory_tile_ids = [];
+                }
+                setTimeout(flip2Back(), 700);
+            }
+        }
+    }
+}
